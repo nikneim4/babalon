e3d1a11dc0f2c7652029a83cad1ebfe274b74f96
PUTIN <PLAYER_1415@mail.ru>
2022-03-26 15:50:23 +0300

    nikneim4

--- dj/blob/views.py
+++ dj/blob/views.py
@@ -7,3 +7,8 @@ def post_list(request):
     return render(request,
                   'blob/post_list.html',
                   {'posteki':posts})# <==
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)# <==
+    return render(request,
+                  'blob/post_detail.html',
+                  {'post':post})# <==
